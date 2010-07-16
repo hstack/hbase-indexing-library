1f9de1487a35efa371f4096c5071b9440131265c
Andrei Dragomir <adragomir@gmail.com>
2010-07-16 14:45:42 +0300

    refactoret index loading methods

--- src/hbaseindex/src/main/java/org/lilycms/hbaseindex/IndexManager.java
+++ src/hbaseindex/src/main/java/org/lilycms/hbaseindex/IndexManager.java
@@ -20,6 +20,8 @@ import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.TableExistsException;
 import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.filter.FilterList;
+import org.apache.hadoop.hbase.filter.PrefixFilter;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.node.ObjectNode;
@@ -45,8 +47,6 @@ public class IndexManager {
   private String dataTableName;
   private HTable metaTable;
   private HTable dataTable;
-
-  private Map<String, Map<String, IndexDefinition>> indexes;
   
   public static final String DEFAULT_META_TABLE = "indexmeta";
   public static final String DEFAULT_DATA_TABLE = "indexdata";
@@ -75,7 +75,6 @@ public class IndexManager {
     this.dataTableName = dataTableName;
     metaTable = new HTable(hbaseConf, this.metaTableName);
     dataTable = new HTable(hbaseConf, this.dataTableName);
-    indexes = new TreeMap<String, Map<String, IndexDefinition>>(); 
   }
 
   /**
@@ -134,28 +133,41 @@ public class IndexManager {
     return index;
   }
 
-  public Map<String, IndexDefinition> getTableIndexes(String table) {
-    return Collections.unmodifiableMap(indexes.get(table));
-  }
-
-  public void loadAllIndexes() throws IOException {
+  private Map<String, Map<String, IndexDefinition>> loadIndexesByPrefix(String prefix) throws IOException {
     Scan scan = new Scan();
     scan.setCaching(10000);
-    metaTable.getScanner(scan);
+
+    if (prefix != null) {
+      scan.setFilter(new PrefixFilter(Bytes.toBytes(prefix)));
+    }
+
+    Map<String, Map<String, IndexDefinition>> toret = new TreeMap<String, Map<String, IndexDefinition>>();
+    
     ResultScanner resScanner = metaTable.getScanner(scan);
     Result[] results = resScanner.next(10000);
     for (Result r: results) {
       // table::index
       String[] ti = Bytes.toString(r.getRow()).split("::");
-      if (!indexes.containsKey(ti[0])) {
-        indexes.put(ti[0], new TreeMap<String, IndexDefinition>());
+      if (!toret.containsKey(ti[0])) {
+        toret.put(ti[0], new TreeMap<String, IndexDefinition>());
       }
       // get the index data, add it to the map
       byte[] jsonData = r.getValue(Bytes.toBytes("meta"), Bytes.toBytes("conf"));
       IndexDefinition indexDef = deserialize(ti[0], ti[1], jsonData);
-      indexes.get(ti[0]).put(ti[1], indexDef);
+      toret.get(ti[0]).put(ti[1], indexDef);
     }
+    return toret;
   }
+
+  public Map<String, Map<String, IndexDefinition>> loadAllIndexes() throws IOException {
+    return loadIndexesByPrefix(null);
+  }
+
+  public Map<String, IndexDefinition> loadTableIndexes(String table) throws IOException {
+    Map<String, Map<String, IndexDefinition>> tmp =  loadIndexesByPrefix(table);
+    return tmp.get(table);
+  }
+
   /**
    * Deletes an index.
    *
